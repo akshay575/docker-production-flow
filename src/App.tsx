f66f4e75b82ab9d813fb710e48abfb664d2488c1
Akshay Arora <akshay575@yahoo.com>
2020-06-07 03:02:58 +0530

    add tests as separate service in docker-compose

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello Docker! Welcome 12!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+ 
