87f3e8dbd7608d53af41c84af3e270ec3eecf65a
Akshay Arora <akshay575@yahoo.com>
2020-06-13 19:58:26 +0530

    update aws deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Docker! Welcome 12!
+          Hello, this is an application to demonstrate complete development lifecycle with Docker!
         </p>
+        <p style={{ margin: 0 }}>It uses below technology stack:</p>
+        <ul style={{ textAlign: 'left' }}>
+          <li>React</li>
+          <li>Docker multi-stage builds</li>
+          <li>Travis CI</li>
+          <li>AWS Elastic Beanstalk</li>
+          <li>AWS S3 Bucket</li>
+          <li>AWS IAM</li>
+        </ul>
       </header>
     </div>
   );
